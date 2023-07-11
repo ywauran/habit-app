428be1deceab4532f083b4a012fb102348840697
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-07-11 20:05:27 +0800

    first commit

---
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -0,0 +1,46 @@
+package com.dicoding.habitapp.ui.list
+
+import android.app.Activity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+class HabitActivityTest {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(HabitListActivity::class.java)
+
+    @Test
+    fun showAddHabit() {
+        onView(withId(R.id.rv_habit)).check(matches(isDisplayed()))
+        onView(withId(R.id.fab)).check(matches(isDisplayed())).perform(click())
+
+        onView(withId(R.id.add_ed_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_ed_minutes_focus)).check(matches(isDisplayed()))
+        onView(withId(R.id.sp_priority_level)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_tv_start_time)).check(matches(isDisplayed()))
+
+        val addHabit = getActivityInstance()
+        assertTrue(addHabit is AddHabitActivity)
+    }
+
+    private fun getActivityInstance(): Activity? {
+        var activity: Activity? = null
+        getInstrumentation().runOnMainSync {
+            activity = ActivityLifecycleMonitorRegistry.getInstance()
+                .getActivitiesInStage(Stage.RESUMED).elementAtOrNull(0)
+        }
+        return activity
+    }
+}
---
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -0,0 +1,46 @@
+package com.dicoding.habitapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
+import java.util.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            //TODO 11: Update theme based on value in ListPreference
+            val themePreference = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+            themePreference?.setOnPreferenceChangeListener { _, newTheme ->
+                val selectedTheme = DarkMode.valueOf(newTheme.toString().toUpperCase(Locale.US))
+                updateTheme(selectedTheme.value)
+                true
+            }
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
+        }
+    }
+
+}
